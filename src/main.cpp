dbab54d700ef611cd80668adec06fa36c9350d55
BuiTienDngg <dungtienbui2004@gmail.com>
2023-10-30 19:53:14 +0700

    checnguoi

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@ bool deviceConnected = false;
 bool oldDeviceConnected = false;
 bool tmpConnected = false;
 bool trueConnected = false;
+bool isReset = false;
+String reset = "RESET";
 BLEServer* pConnectedServer = NULL; 
 std:: string receiveData;
 std:: string receiveID = "";
@@ -40,18 +42,19 @@ class MyServerCallbacks: public BLEServerCallbacks {
 };
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
-      receiveID = pCharacteristic->getValue();
+      
       if(trueConnected == false){
+        receiveID = pCharacteristic->getValue();
           if (receiveID.length() > 0) {
-            Serial.print("New value: ");
+            Serial.print("New ID value: ");
             for (int i = 0; i < receiveID.length(); i++){
               Serial.print(receiveID[i]);
-              Serial.println();
-             }
+             }Serial.println();
         }
       }else{
+          receiveData = pCharacteristic->getValue();
           if (receiveData.length() > 0) {
-              Serial.print("New value: ");
+              Serial.print("New data value: ");
               for (int i = 0; i < receiveData.length(); i++){
                 Serial.print(receiveData[i]);
                 Serial.println();
@@ -64,13 +67,9 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 void setup() {
   Serial.begin(115200);
   EEPROM.begin(EEPROM_SIZE);
-  for(int i = 0;i < 8; i++){
-    EEPROM.write(i,0);
-  }
   pinMode(button,INPUT_PULLDOWN);
   Serial.println("Starting BLE work!");
-
-  BLEDevice::init("Long name works now");
+  BLEDevice::init("Dũngg");
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
   pService = pServer->createService(SERVICE_UUID);
@@ -88,14 +87,14 @@ void setup() {
   pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
   pAdvertising->setMinPreferred(0x12);
   BLEDevice::startAdvertising();
-  writeStringToEEPROM(0,ID);
+  // writeStringToEEPROM(0,Default);
   Serial.print("EEPROM hiện tại là ");
   Serial.println(readStringFromEEPROM(0,8));
   delay(2000);
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 void loop() {
-  if(tmpConnected  && !trueConnected ){ // khi kết nối với app, mở ra 1 connect tạm thời để check ID
+  if(tmpConnected  && !trueConnected){ // khi kết nối với app, mở ra 1 connect tạm thời để check ID
       if(isFirstDevice()){// tức là ID trong EEPROM là 00000000
               Serial.println("lần đầu pair");
               if(receiveID.length() > 0){
@@ -103,34 +102,35 @@ void loop() {
                 EEPROM.commit();
                 tmpConnected = false;
                 trueConnected = true;
+                Serial.println("cho phép pair");
+                pCharacteristic->setValue("CONNECT OK");
               } 
       }else{ // ID trong EEPRom khac 00000000 vi du 01234567
               Serial.println("Đã mất trinh ");
               delay(1000);
               if(receiveID.length() > 0){
-                String tmp = receiveID.c_str();
-                if( tmp == readStringFromEEPROM(0,8)){ // so sánh chuỗi nhận được với trong eeprom
-                Serial.print(tmp);Serial.print("   ");
-                Serial.println(readStringFromEEPROM(0,8));
-                delay(1000);
-                trueConnected = true;
-                tmpConnected = false;
-                Serial.println("cho phép pair");
-                pCharacteristic->setValue("CONNECT OK");
-
-                delay(1000);
+                      String tmp = receiveID.c_str();
+                      if( tmp == readStringFromEEPROM(0,8)){ // so sánh chuỗi nhận được với trong eeprom
+                      Serial.print(tmp);Serial.print("   ");
+                      Serial.println(readStringFromEEPROM(0,8));
+                      delay(1000);
+                      trueConnected = true;
+                      tmpConnected = false;
+                      Serial.println("cho phép pair");
+                      pCharacteristic->setValue("CONNECT OK");
+                      delay(1000);
                 }else{
-                  Serial.println("Không cho phép pair");
-                  pCharacteristic->setValue("CONNECT FAIL");
-                  delay(TIME_TO_DISCONNECT);
-                  pConnectedServer->disconnect(pConnectedServer->getConnectedCount());
-                  pConnectedServer = NULL;
-                  // ngắt kết nối 
-                  delay(1000);
+                      Serial.println("Không cho phép pair");
+                      pCharacteristic->setValue("CONNECT FAIL");
+                      delay(TIME_TO_DISCONNECT);
+                      pConnectedServer->disconnect(pConnectedServer->getConnectedCount());
+                      pConnectedServer = NULL;
+                      // ngắt kết nối 
+                      delay(1000);
                 } 
               }
-      } Serial.println("Đợi APP gửi ID");
-  }else{ //  đã kết nối tới app, bắt đầu gửi bước chân 
+      }
+  }else { //  đã kết nối tới app, bắt đầu gửi bước chân 
     if(digitalRead(button) == 1){
       delay(50);
       while(digitalRead(button) == 1);
@@ -138,24 +138,23 @@ void loop() {
       std::string V = intToString(value);
       pCharacteristic->setValue(V);
     }
+    if((String)receiveData.c_str() == reset){
+      writeStringToEEPROM(0,Default);
+      Serial.println("RESET");
+      //ngắt kết nối
+      delay(TIME_TO_DISCONNECT);
+      pConnectedServer->disconnect(pConnectedServer->getConnectedCount());
+      pConnectedServer = NULL;
+    }
   }
+  // }else if(isReset){
+  //   writeStringToEEPROM(0,Default);
+  //   Serial.println("RESET");
+  //   //ngắt kết nối
+  //   delay(TIME_TO_DISCONNECT);
+  //   pConnectedServer->disconnect(pConnectedServer->getConnectedCount());
+  //   pConnectedServer = NULL;
 
-      
-      // if(receiveData.length() > 0){ // nhận được ID từ app
-      //     else if(isEquals(receiveData,EEPROM.readString(0))){ // nếu ID nhận được = ID trong EEPROOM
-      //         Serial.println("ID đã nhận bằng ID trong eeprom, cho phép kết nối");
-      //     }else Serial.println("ID không trùng khớp ");
-      // }else Serial.println("đợi APP gửi ");
-  // }
-
-  // put your main code here, to run repeatedly:
-  // if (deviceConnected && !isFirstDevice()) { // kết nối và kiểm tra ID gửi về từ APP
-  //       Serial.println(EEPROM.readString(0));
-  //       delay(1000);
   // }
   
-    //pCharacteristic->setValue((uint8_t*)&value, 4);
-        // pCharacteristic->notify();
-        // value++;
-        // delay(1000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
 }
